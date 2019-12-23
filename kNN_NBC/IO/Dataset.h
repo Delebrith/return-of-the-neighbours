f94332062b02dc4fb41607d615f9cabecf92a934
lychanl <lycha_@poczta.onet.pl>
2019-12-23 18:08:10 +0100

    CSV reading

---
+++ kNN_NBC/IO/Dataset.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <string>
+
+
+template<class T> class Dataset
+{
+public:
+	Dataset(Dataset&& d) noexcept
+		: ids(std::move(d.ids)), features(std::move(d.features)), _hasIds(d._hasIds) {}
+	explicit Dataset(std::vector<std::vector<T>>&& features)
+		: features(features), _hasIds(false) {}
+	Dataset(std::vector<std::string>&& ids, std::vector<std::vector<T>>&& features)
+		: ids(ids), features(features), _hasIds(true) {}
+
+	std::vector<std::vector<T>>& getFeatures();
+	std::vector<std::string>& getIds();
+	bool hasIds();
+
+	static Dataset readCsv(
+		std::istream& stream,
+		bool headers = true,
+		int idCol = -1,
+		const std::set<int>& ignoreCols = {},
+		char delim = ',',
+		char enclosing = '"');
+
+	void writeCsv(
+		std::ostream& stream,
+		char delim = ',');
+
+private:
+	Dataset() {}
+	Dataset(Dataset&) {}
+	std::vector<std::string> ids;
+	std::vector<std::vector<T>> features;
+	bool _hasIds;
+
+	static bool readCsvRow(std::istream& stream, std::string& buffer, char enclosing);
+	static std::vector<std::string> parseCsvRow(std::string& line, char delim, char enclosing);
+	static std::string unescapeCsv(std::string field, char enclosing);
+	static T parse(const std::string&);
+};
--- kNN_NBC/IO/main.cpp
+++ kNN_NBC/IO/main.cpp
@@ -5,12 +5,45 @@
 // check tclap parser inclusion
 #include <tclap/CmdLine.h>
 
+#include "Dataset.h"
+
+#include <iostream>
 
 
-int main()
+int main(int argc, char** argv)
 {
-	assert(fun(1) == 1);
-	assert(fun_negate({ 1, 2, 3 }).size() == 3);
+	TCLAP::CmdLine args("CLI for implementation of NBC algorithm with kNN and k+NN");
+	TCLAP::ValueArg<char> delimArg("d", "csv-delim", "A delimiter for CSV file", false, ',', "char");
+	TCLAP::SwitchArg delimTabArg("t", "csv-delim-tab", "Use tab as a delimiter for CSV file. Has priority over csv-delim");
+	TCLAP::ValueArg<char> enclosingArg("e", "enclosing", "An enclosing character for CSV file", false, '"', "char");
+	TCLAP::SwitchArg headersArg("r", "headers", "Indicates that csv file contains headers row that is to be ignored");
+	TCLAP::ValueArg<int> idColArg("i", "id", "Indicates that csv file contains id column", false, -1, "int");
+	TCLAP::MultiArg<int> ignoreColsArg("n", "ignore", "Specifies columns to be ignored by algorithm", false, "int");
+
+	args.add(delimArg);
+	args.add(delimTabArg);
+	args.add(enclosingArg);
+	args.add(headersArg);
+	args.add(idColArg);
+	args.add(ignoreColsArg);
+
+	args.parse(argc, argv);
+
+	char delim = delimTabArg.getValue() ? '\t' : delimArg.getValue();
+	bool headers = headersArg.getValue();
+	int idCol = idColArg.getValue();
+	std::set<int> ignoreCols(ignoreColsArg.getValue().cbegin(), ignoreColsArg.getValue().cend());
+	char enclosing = enclosingArg.getValue();
+
+	Dataset<double> d(Dataset<double>::readCsv(std::cin, headers, idCol, ignoreCols, delim, enclosing));
+
+	for (std::vector<double> f : d.getFeatures())
+	{
+		for (double v : f)
+			std::cout << v << "\t";
+		std::cout << std::endl;
+	}
 
 	return 0;
-}	
+}
+
