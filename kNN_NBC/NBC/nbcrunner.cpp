727e3b9a3dbdcfe953112e50247f1d6f6d8c547c
lychanl <lycha_@poczta.onet.pl>
2020-01-05 02:26:21 +0100

    Merge

--- kNN_NBC/NBC/nbcrunner.cpp
+++ kNN_NBC/NBC/nbcrunner.cpp
@@ -5,17 +5,19 @@
 #include <algorithm>
 
 NBCRunner::NBCRunner(int k, const ReferenceStrategy referenceStrategy, 
-	const std::vector<std::vector<double>>& features) 
+	const std::vector<std::vector<double>>& features, bool enableParallel)
 {
 	this->k = k;
 	this->referencePoint = selectReferencePoint(features, referenceStrategy);
 	this->points = initPoints(features);
+	this->enableParallel = enableParallel;
 }
 
 std::vector<int> NBCRunner::run()
 {
-	for (Point& p : this->points)
-		p.setDistanceFromReference(this->calculateDistanceToReferencePoint(p));
+#pragma omp parallel for if(this->enableParallel)
+	for (int i = 0; i < this->points.size(); ++i)
+		this->points[i].setDistanceFromReference(this->calculateDistanceToReferencePoint(this->points[i]));
 
 	std::vector<Point*> orderedByDistToReference = this->getPointsOrderedByDistanceToReference();
 	this->calculateNeighborhoods(orderedByDistToReference);
@@ -56,6 +58,7 @@ std::vector<Point*> NBCRunner::getPointsOrderedByDistanceToReference()
 
 void NBCRunner::calculateNeighborhoods(std::vector<Point*>& order)
 {
+#pragma omp parallel for if(this->enableParallel)
 	for (int i = 0; i < this->points.size(); ++i)
 		this->calculatePointNeighborhood(i, order);
 }
@@ -131,8 +134,9 @@ void NBCRunner::calculateReversedNeighbourhoods()
 
 void NBCRunner::calculateNDFs()
 {
-	for (DatasetPoint& point : this->points) {
-		point.calculateNDF();
+#pragma omp parallel for if(enableParallel)
+	for (int i = 0; i < this->points.size(); ++i) {
+		this->points[i].calculateNDF();
 	}
 }
 
